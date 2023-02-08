437d8731ef6a7ff1f0617f965f5f244ffe71ce70
Annas Abdurrahman <68950058+annasabdurrahman354@users.noreply.github.com>
2023-02-08 12:01:09 +0700

    Succes Adding User Pelayanan

--- app/Http/Livewire/User/Pelayanan/PelayananCreate.php
+++ app/Http/Livewire/User/Pelayanan/PelayananCreate.php
@@ -22,22 +22,11 @@ class PelayananCreate extends Component
     public $inputs = [];
     public array $listsForFields = [];
 
-    public $validatorPelayanan;
-    public $validatorInputs;
-
-    public $rulesPelayanan = [
+    protected $rules = [
         'pelayanan.jenis_layanan_id' => 'required|integer|exists:jenis_layanans,id',
         'pelayanan.kode' => 'required|string',
         'pelayanan.catatan_pemohon' => 'string|nullable'
     ];
-    public $rulesInputs = [];
-
-    public function rules() 
-    {
-        return [
-            'pelayanan.catatan_pemohon' => 'string|nullable'
-        ];
-    }
 
     public function mount()
     {
@@ -53,42 +42,56 @@ class PelayananCreate extends Component
 
     public function submit()
     {
-        $this->pelayanan->jenis_layanan_id = $this->jenis;
-        $this->validatorInputs = Validator::make($this->inputs, $this->rulesInputs);
         
-        if ($this->validatorInputs->fails()) {
-            
-        } 
-        else {
-            $this->validatorPelayanan = Validator::make($this->pelayanan->toArray(), $this->rulesPelayanan);
-            if ($this->validatorPelayanan->fails()) {
-            
+        $this->validate();
+        $this->pelayanan->jenis_layanan_id = $this->jenis;
+        $this->pelayanan->pemohon_id = auth()->user()->id;
+        $this->pelayanan->status = "Terkirim";
+        
+        $r = [
+            'pelayanan.jenis_layanan_id' => 'required|integer|exists:jenis_layanans,id',
+            'pelayanan.kode' => 'required|string',
+            'pelayanan.catatan_pemohon' => 'string|nullable'
+        ];
+
+        $rules = [];
+        foreach ($this->syarats as $index => $syarat){
+            if ( $syarat['jenis_berkas'] === 'Foto') 
+            {
+                $rules['inputs.'.$index] = 'image|mimes:jpg,png|required';
             } 
-            else{
-                $this->pelayanan->generateCode();
-                $this->pelayanan->pemohon_id = auth()->user()->id;
-                $this->pelayanan->status = "Terkirim";
-                foreach ($this->syarats as $index => $syarat){
-                    $berkas_pelayanan = new BerkasPelayanan();
-                    $berkas_pelayanan->pelayanan_id = $this->pelayanan->id;
-                    $berkas_pelayanan->syarat_layanan_id = $syarat->id;
-                    $berkas_pelayanan->status = "Verifikasi";
-                    if ($syarat->jenis_berkas == 'Teks') 
-                    {
-                        $berkas_pelayanan->teks_syarat = $this->inputs[$index];
-                    } 
-                    else 
-                    {
-                        $berkas_pelayanan->addMedia($this->inputs[$index]->getRealPath())
-                                        ->usingName(Str::slug($this->pelayanan->id."-".$berkas_pelayanan->id."-".$syarat->nama, '-'))
-                                        ->toMediaCollection("berkas_pelayanan_berkas_syarat");
-                    } 
-                    $berkas_pelayanan->save();
-                }
-                $this->pelayanan->save();
-                return redirect()->route('user.pelayanan');
+            elseif ($syarat['jenis_berkas'] === 'Dokumen') 
+            {
+                $rules['inputs.'.$index] = 'file|required|mimetypes:application/msword,application/pdf,application/vnd.ms-excel';
+            } 
+            elseif ($syarat['jenis_berkas'] === 'Teks') 
+            {
+                $rules['inputs.'.$index] = 'string|required';
             }
         }
+
+        $this->validate(array_merge($r, $rules));
+
+        foreach ($this->syarats as $index => $syarat){
+            $berkas_pelayanan = new BerkasPelayanan();
+            $berkas_pelayanan->pelayanan_id = $this->pelayanan->id;
+            $berkas_pelayanan->syarat_layanan_id = $syarat->id;
+            $berkas_pelayanan->status = "Verifikasi";
+            
+            if ($syarat->jenis_berkas == 'Teks') 
+            {
+                $berkas_pelayanan->teks_syarat = $this->inputs[$index];
+            } 
+            else 
+            {
+                $berkas_pelayanan->addMedia($this->inputs[$index]->getRealPath())
+                                ->usingName(Str::slug($this->pelayanan->id."-".$berkas_pelayanan->id."-".$syarat->nama, '-'))
+                                ->toMediaCollection("berkas_pelayanan_berkas_syarat");
+            } 
+            $berkas_pelayanan->save();
+        }
+        $this->pelayanan->save();
+        return redirect()->route('user.pelayanan');
     }
 
     protected function initListsForFields(): void
@@ -99,6 +102,8 @@ class PelayananCreate extends Component
     public function updatedJenis($value)
     {
         $this->pelayanan->jenis_layanan_id = $value;
+        $this->pelayanan->generateCode();
+
         $this->pelayanan->load('jenisLayanan.syaratLayanan')->get;
         $this->syarats = SyaratLayanan::whereHas('jenislayanan', function ($query) {
                             $query->where('jenis_layanan_id',$this->pelayanan->jenis_layanan_id);
@@ -106,20 +111,6 @@ class PelayananCreate extends Component
         $this->inputs = array();
         foreach ($this->syarats as $i) {
             array_push($this->inputs, "");
-        }
-        foreach ($this->syarats as $index => $syarat){
-            if ( $syarat['jenis_berkas'] === 'Foto') 
-            {
-                $this->rulesInputs[$index] = 'image|mimes:jpg,png|required';
-            } 
-            elseif ($syarat['jenis_berkas'] === 'Dokumen') 
-            {
-                $this->rulesInputs[$index] = 'file|required|mimetypes:application/msword,application/pdf,application/vnd.ms-excel';
-            } 
-            elseif ($syarat['jenis_berkas'] === 'Teks') 
-            {
-                $this->rulesInputs[$index] = 'string|required';
-            }
         }
     }
 }
--- resources/views/livewire/user/pelayanan/create.blade.php
+++ resources/views/livewire/user/pelayanan/create.blade.php
@@ -12,14 +12,13 @@
         </div>
         <div class="form-group {{ $errors->has('pelayanan.catatan_pemohon') ? 'invalid' : '' }}">
             <label class="form-label" for="catatan_pemohon">Sisipkan Catatan</label>
-            <input type="text" name="catatan_pemohon" id="catatan_pemohon" class="user-input-text" placeholder="Tuliskan catatan Anda untuk petugas" wire:model="pelayanan.catatan_pemohon">
+            <input type="text" name="catatan_pemohon" id="catatan_pemohon" class="input-textarea" placeholder="Tuliskan catatan Anda untuk petugas" wire:model="pelayanan.catatan_pemohon">
             <div class="validation-message">
                 {{ $errors->first('pelayanan.catatan_pemohon') }}
             </div>
             <div class="help-block">
                 {{ trans('cruds.pelayanan.fields.catatan_pemohon_helper') }}
             </div>
-            {{$pelayanan->catatan_pemohon}}
         </div>
         @if ($jenis)
         <div class="form-group {{ $errors->has('pelayanan.berkas_syarat') ? 'invalid' : '' }}">
@@ -28,10 +27,11 @@
             <div :key="syarat_{{ $index }}">
                 <h2>
                     <div class="flex items-center justify-between w-full p-2 font-medium text-left border border-gray-200 dark:border-gray-700 bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-white" >
-                    <span class="flex items-center">                
+                    <span class="flex items-center @error('inputs.' . $index) {{'text-red-500'}} @enderror">                
                         <svg class="w-5 h-5 mr-2 shrink-0"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M14 3v4a1 1 0 0 0 1 1h4" />  <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />  <line x1="9" y1="7" x2="10" y2="7" />  <line x1="9" y1="13" x2="15" y2="13" />  <line x1="13" y1="17" x2="15" y2="17" /></svg>
                         {{ $syarat['nama'] }}
                         </span>
+                        
                     </div>
                 </h2>
                 <div >
@@ -42,50 +42,45 @@
 
                         @if(!$syarat->berkas_formulir->isEmpty())
                         @foreach($syarat['berkas_formulir'] as $key => $entry)
-                            <a class="link-light-blue mb-2" href="{{ $entry['url'] }}">
+                            <a class="link-light-blue mb-4 mt-2" href="{{ $entry['url'] }}">
                                 <i class="far fa-file">
                                 </i>
-                                Formulir pengisian : {{ $entry['file_name'] }}
+                                Formulir: {{ $entry['file_name'] }}
                             </a>
                         @endforeach
                         @endif
                         <div class="form-group">
                             @if($syarat['jenis_berkas'] === 'Teks')
                                 <label class="form-label" for="teks_syarat_{{$index}}">{{ trans('cruds.berkasPelayanan.fields.teks_syarat') }}</label>
-                                <input wire:model="inputs.{{ $index }}" :key="syarat_{{ $index }}_input" class="form-control" type="text" name="teks" id="teks">
-                                <div class="help-block">
-                                    {{ trans('cruds.berkasPelayanan.fields.teks_syarat_helper') }}
-                                </div>
-                                 {{$this->inputs[$index]}}
+                                <input wire:model="inputs.{{ $index }}" :key="syarat_{{ $index }}_input" class="input-text" type="text" name="teks" id="teks">
                             @elseif($syarat['jenis_berkas'] === 'Dokumen')
-                                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file">Pilih Berkas</label>
+                                <label class="form-label" for="file">Pilih Berkas</label>
                                 <input wire:model="inputs.{{ $index }}" :key="syarat_{{ $index }}_input" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="file" name="file" type="file" accept=".xlsx, .xls, .doc, .docx, .ppt, .pptx, .pdf">
                             @elseif($syarat['jenis_berkas'] === 'Foto')
-                                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file">Pilih Foto</label>
+                                <label class="form-label" for="file">Pilih Foto</label>
                                 <input wire:model="inputs.{{ $index }}" :key="syarat_{{ $index }}_input" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="file" name="file" type="file" accept="image/png, image/jpeg">
                             @endif
-                        <div class="validation-message">
-                            {{ $validatorB->errors() ? $validatorB->errors()->first('inputs'.$index): ""}}
-                        </div>
+                            @error('inputs.' . $index) <span class="error input-error">{{ str_replace('inputs.' . $index, $syarat['nama'], $message) }}</span> @enderror
                         </div>
                     </div>
                 </div>
             </div>
-            @endforeach
-
+            @endforeach 
         </div>
         @endif
-        
         <div class="form-group">
-            <div wire:loading.inline-flex >
-                <x-loading/> 
-                <p>Loading</p> 
+            <div wire:loading >
+                <div class="flex justify-start">
+                    <x-loading class="m-auto"/> 
+                    <p class="m-auto">Loading</p>
+                </div>
             </div>
             <div wire:loading.remove>
                 <button type="button" wire:click="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Ajukan</button>
                 <a href="{{ route('user.pelayanan') }}" type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Batal</a>
             </div>
-        </div>        
+        </div>   
+
     </div>
     
 </div>
