43a691d092727d0e24b0a945cbb91d113314c3c3
Annas Abdurrahman <68950058+annasabdurrahman354@users.noreply.github.com>
2023-03-05 21:53:26 +0700

    Changing Guest Header Contact Label

--- resources/views/components/guest-header.blade.php
+++ resources/views/components/guest-header.blade.php
@@ -5,11 +5,11 @@
           <svg class="w-5 h-5 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"/>
           </svg>
-          <span class="text-xs md:text-sm">+6285786537295</span>
+          <span class="text-xs md:text-sm">{{getKontak(6)->kontak}}</span>
       </a>
       <a href="mailto:{{getKontak(2)->kontak}}" class="text-white hover:text-gray-200 hover:underline flex gap-1 items-center">
           <svg class="w-5 h-5 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />  <polyline points="22,6 12,13 2,6" /></svg>
-          <span class="text-xs md:text-sm">web.srimulyo@gmail.com</span>
+          <span class="text-xs md:text-sm">{{getKontak(2)->kontak}}</span>
       </a>
     </div>
     <div class="hidden sm:flex gap-2">
